a09f5cf5517fe1b0740ec7f89586cc7d39e233c1
marogosteen <>
2023-06-27 10:55:50 +0000

    refactor post sort

--- blog-site/src/components/PostsByYear.svelte
+++ blog-site/src/components/PostsByYear.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
     import type { CollectionEntry } from "astro:content";
     import Card from "./Card.svelte";
+    import getSortedPosts from "@utils/getSortedPosts";
 
     export let year: number;
     export let posts: CollectionEntry<"blog">[];
+    posts = getSortedPosts(posts);
 </script>
 
 <li class="my-6">
     <div class="flex flex-col sm:flex-row gap-6">
-        <h3 class="basis-1/6 text-2xl sm:text-3xl font-extrabold">{year}</h3>
+        <h3 class="basis-1/6 text-2xl sm:text-3xl font-bold">{year}</h3>
         <ul class="basis-5/6">
             {#each posts as post}
                 <Card {post} />
