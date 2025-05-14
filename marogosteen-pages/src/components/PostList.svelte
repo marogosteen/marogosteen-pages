67b1e8e5adcd3ea29104c2300fea3952cfd69b6c
marogosteen <marogosteen@gmail.com>
2025-05-14 04:40:33 +0000

    feat: 検索ページと post 一覧ページの統合

--- marogosteen-pages/src/components/PostList.svelte
+++ marogosteen-pages/src/components/PostList.svelte
@@ -4,8 +4,10 @@
     import getSortedPosts from "@utils/getSortedPosts";
     import getPostsByYearMap from "@utils/getPostsByYearMap";
 
-    export let title: String;
     export let allPosts: CollectionEntry<"blog">[];
+
+    let query = "";
+    let searchResult: CollectionEntry<"blog">[] = [];
 
     allPosts = getSortedPosts(allPosts);
     const pagerMaxSize = 7;
@@ -24,12 +26,23 @@
 
     const getDisplayPosts = (
         posts: CollectionEntry<"blog">[],
-        currentPage: number
+        currentPage: number,
     ): CollectionEntry<"blog">[] => {
         currentPage = currentPage;
         const start = currentPage * cardMaxDisplay;
         const end = (currentPage + 1) * cardMaxDisplay;
         return getSortedPosts(posts.slice(start, end));
+    };
+
+    const searchHandler = () => {
+        let lq = query.toLowerCase();
+        displayPosts = allPosts.filter((post) => {
+            return (
+                post.data.title.toLowerCase().includes(lq) ||
+                post.data.description.toLowerCase().includes(lq) ||
+                post.body.toLowerCase().includes(lq)
+            );
+        });
     };
 
     let currentPage = 0;
@@ -38,7 +51,29 @@
 </script>
 
 <div class="flex flex-col w-full">
-    <h2 class="text-3xl font-bold my-10">{title}</h2>
+    <div class="flex w-full mb-8 rounded-lg py-2 bg-gray-700">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-search my-auto mx-3"
+            viewBox="0 0 16 16"
+        >
+            <path
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+            ></path>
+        </svg>
+
+        <input
+            id="search-box"
+            type="text"
+            on:change={searchHandler}
+            bind:value={query}
+            class="input text-lg px-5 w-full focus:outline-none"
+        />
+    </div>
+
     <div class="join flex justify-center">
         {#each paginationPages as page}
             {#if page === currentPage}
@@ -72,7 +107,10 @@
                     {page + 1}
                 </button>
             {:else}
-                <button class="px-2 my-6 border-x" on:click={() => (currentPage = page)}>
+                <button
+                    class="px-2 my-6 border-x"
+                    on:click={() => (currentPage = page)}
+                >
                     {page + 1}
                 </button>
             {/if}
--- marogosteen-pages/src/components/PostsByYear.svelte
+++ marogosteen-pages/src/components/PostsByYear.svelte
@@ -10,8 +10,8 @@
 
 <li class="my-6">
     <div class="flex flex-col sm:flex-row gap-6">
-        <h3 class="basis-1/6 text-2xl sm:text-3xl font-bold">{year}</h3>
-        <ul class="basis-5/6">
+        <p class="w-18 text-xl sm:text-2xl font-bold">{year}</p>
+        <ul>
             {#each posts as post}
                 <Card post={post} />
             {/each}
