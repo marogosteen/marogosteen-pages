a414a5b690f8f4b4259ef57b11922b4c4c2c2e06
marogosteen <marogosteen@gmail.com>
2025-05-29 16:42:53 +0000

    debug: oembedTransformer

--- marogosteen-pages/astro.config.mjs
+++ marogosteen-pages/astro.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
 import remarkToc from 'remark-toc'
+import oembedTransformer from '@remark-embedder/transformer-oembed'
+import remarkEmbedder from '@remark-embedder/core'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
 
   markdown: {
-    remarkPlugins: [[remarkToc, { heading: "contents" }]],
+    remarkPlugins: [
+      [remarkToc, { heading: "contents" }],
+      [remarkEmbedder.default, { transformers: [oembedTransformer] }]
+    ],
   },
 
   vite: {
